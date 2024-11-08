a327a11f0d4e13a97f67ef44674fe62926fa9e15
joninet2971 <j.desplats@itecriocuarto.org.ar>
2024-11-08 18:23:43 -0300

    Update marca_view.py

--- views/marca_view.py
+++ views/marca_view.py
@@ -13,6 +13,8 @@ from schemas import MarcaSchema
 
 marca_bp = Blueprint('marca', __name__)
 
+from base64 import b64decode
+
 @marca_bp.route('/marca', methods=['GET','POST'])
 @jwt_required()
 def marca():
@@ -23,6 +25,7 @@ def marca():
     admin = additional_data.get('administrador')
 
     if request.method == 'POST':
+        print("aca")
         if  admin:
             data = request.get_json()
             errors = MarcaSchema().validate(data)
@@ -37,10 +40,31 @@ def marca():
             return MarcaSchema().dump(nueva_marca), 201
         else:
             return jsonify(Mensaje= "Solo el admin puede crear nuevas marcas")
-    
     if visor or admin:
         return MarcaSchema().dump(marca, many=True)
     return jsonify(Mensaje="El usuario no tiene permiso")
+
+@marca_bp.route('/marca/deactivate', methods=['POST'])
+@jwt_required()
+def deactivate_marca():
+    data = request.get_json()
+    marca_id = data.get('marca_id')
+
+    marca = Marca.query.get_or_404(marca_id)
+    
+    if marca.activo:
+        additional_data = get_jwt()
+        administrador = additional_data.get('administrador')
+        editor = additional_data.get('editor')
+
+        if administrador or editor:
+            marca.activo = False
+            db.session.commit()
+            return jsonify({"Mensaje": "marca borrado correctamente", "Usuario": marca.nombre})
+        else:
+            return jsonify({"Mensaje": "No tienes permisos para borrar marca"}), 401
+    else:
+        return jsonify({"Mensaje": "Esta marca ya estaba borrado", "Usuario": marca.nombre}), 401
 
 @marca_bp.route('/marca/borrar', methods=['POST'])
 @jwt_required()
@@ -72,19 +96,21 @@ def marca_borrar():
 @marca_bp.route('/marca/editar', methods=['POST'])
 @jwt_required()
 def marca_editar():
+    additional_data = get_jwt()
     data = request.get_json()
-    id_editar = int(data.get('id'))
+    id_editar = data.get('id')
     nombre_editar = data.get('nombre')
 
     marca = Marca.query.get_or_404(id_editar)
+    print("1")
     
     if marca.activo is True:
         additional_data = get_jwt()
         editor = additional_data.get('editor')
         admin = additional_data.get('administrador')
-
+        print("2")
         if editor or admin:
-            
+            print("3")
             
             """errors = MarcaSchema().validate(data)
             if errors:
